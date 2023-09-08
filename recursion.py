8e493bfa5f984bcebc14fde84d9347e8fe9e098f
srihariharan9982 <144433288+srihariharan9982@users.noreply.github.com>
2023-09-08 14:45:37 +0530

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,17 @@
+#python 3 program to find
+# factorial of given number\
+def factorial(n):
+ifn<0:
+return 0
+elif n==0 or n==1:
+return 1
+else:
+fact=1 
+while(n>1):
+fact*=n
+n-=1
+return fact
+# driver code
+num=5
+print("factorial of".num."is".
+factorial(num))
